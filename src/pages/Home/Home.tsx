2450eac5748e175886c0cda385398aa72e975871
MathildeDuboille <duboille.m@gmail.com>
2021-09-29 09:29:40 +0200

    use user information to display welcome message

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,26 @@
+import { UserClaims } from '@okta/okta-auth-js';
 import { useOktaAuth } from '@okta/okta-react';
 import Button from 'components/Button';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { HomeContainer } from './Home.style';
 
 const Home: React.FC = () => {
   const { authState, oktaAuth } = useOktaAuth();
+  const [userInfo, setUserInfo] = useState<null | UserClaims>(null);
 
   const login = async () => {
     oktaAuth.signInWithRedirect({ originalUri: '/' });
   };
+
+  useEffect(() => {
+    if (!authState || !authState.isAuthenticated) {
+      setUserInfo(null);
+    } else {
+      oktaAuth.getUser().then(info => {
+        setUserInfo(info);
+      });
+    }
+  }, [authState, oktaAuth]);
 
   if (!authState || !authState.isAuthenticated) {
     return (
@@ -18,11 +30,15 @@ const Home: React.FC = () => {
     );
   }
 
-  return (
-    <HomeContainer>
-      <div>Welcome, you are now authenticated !</div>
-    </HomeContainer>
-  );
+  if (authState.isAuthenticated && !userInfo) {
+    return <HomeContainer>Loading user information...</HomeContainer>;
+  }
+
+  if (authState.isAuthenticated && userInfo) {
+    return <HomeContainer>Welcome {userInfo.name}!</HomeContainer>;
+  }
+
+  return null;
 };
 
 export default Home;
